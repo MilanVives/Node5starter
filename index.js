61d2779125dea581bde4d1d32ea5cf7292d5b338
Milan <milan@MDMBA.localdomain>
2022-03-19 18:54:09 +0100

    Joi import verwijderd in index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const Joi = require('joi');
 const logger = require('./middleware/logger');
 const helmet = require('helmet');
 const morgan = require('morgan');
